16d8be1a82f5308fd4e5405adb1b01069089ff49
Mikkomannine <mikktma@metropolia.fi>
2024-04-17 10:09:18 +0300

    firstpush

---
+++ src/src/PalindromeChecker.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String input = scanner.nextLine();
+        scanner.close();
+
+        String transformed = transformInput(input);
+        boolean isPalindrome = checkPalindrome(transformed);
+
+        if (isPalindrome) {
+            System.out.println("The transformed input is a palindrome.");
+        } else {
+            System.out.println("The transformed input is not a palindrome.");
+        }
+    }
+
+    public static String transformInput(String input) {
+        return input.toLowerCase().replaceAll("[,!]", "");
+    }
+
+    public static boolean checkPalindrome(String input) {
+        String reversed = reverseString(input);
+        return input.equals(reversed);
+    }
+
+    public static String reverseString(String input) {
+        StringBuilder reversed = new StringBuilder(input.length());
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed.append(input.charAt(i));
+        }
+        return reversed.toString();
+    }
+}
